607025728ac039e0463b325aea6dd2cd853c6f73
beezee <brian.zeligson@gmail.com>
2012-03-24 14:33:18 -0400

    sanitizing all data

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'uri'
 require 'mongo'
 require 'base64'
 require 'json'
+require 'sanitize'
 
 if (ENV['MONGOHQ_URL'])
   uri = URI.parse(ENV['MONGOHQ_URL'])
@@ -23,15 +24,18 @@ end
 get '/stats/:token/:event' do
   #TODO: factor out so this can reuse code from property route
   coll = db.collection MONGO_COLL
+  event = Sanitize.clean(params[:event])
+  token = Sanitize.clean(params[:token])
+  
   result = []
-  coll.find({"event" => "#{params[:event]}", "properties.token" => "#{params[:token]}"}).each {|row| result.push row}
+  coll.find({"event" => "#{event}", "properties.token" => "#{token}"}).each {|row| result.push row}
   if not params[:token] == 'chtkmpdemo'
   #TODO: add formal authentication/registration/all that good stuff
     result = {}
     result['status'] = 'failed'
     result['reason'] = 'unauthorized'
   elsif not result.length > 0
-    result = {'status' => 'failed', 'reason' => 'No results matched your query', 'query' => "event #{params[:event]}, property: #{params[:property]}"}
+    result = {'status' => 'failed', 'reason' => 'No results matched your query', 'query' => "event: #{event}"}
   end
   content_type :json
   result.to_json
@@ -40,14 +44,18 @@ end
 get '/stats/:token/:event/:property' do
   coll = db.collection MONGO_COLL
   result = []
-  coll.find({"event" => "#{params[:event]}", "properties.#{params[:property]}" => {'$exists' => true}, "properties.token" => "#{params[:token]}"}).each {|row| result.push row}
+  event = Sanitize.clean(params[:event])
+  property = Sanitize.clean(params[:property])
+  token = Sanitize.clean(params[:token])
+  coll.find({"event" => "#{event}", "properties.#{property}" => {'$exists' => true}, "properties.token" => "#{token}"}).each {|row| result.push row}
   if not params[:token] == 'chtkmpdemo'
   #TODO: add formal authentication/registration/all that good stuff
     result = {}
     result['status'] = 'failed'
     result['reason'] = 'unauthorized'
   elsif not result.length > 0
-    result = {'status' => 'failed', 'reason' => 'No results matched your query', 'query' => "event #{params[:event]}, property: #{params[:property]}"}
+    #convert params to strings re: http://www.idontplaydarts.com/2010/07/mongodb-is-vulnerable-to-sql-injection-in-php-at-least/
+    result = {'status' => 'failed', 'reason' => 'No results matched your query', 'query' => "event: #{event}, property: #{event}"}
   end
   content_type :json
   result.to_json
@@ -69,9 +77,12 @@ get '/track/' do
     result['status'] = 'failed'
     result['reason'] = 'no ip provided'
   else
-    data['mpclone_time_tracked'] = Time.now.to_i
     coll = db.collection MONGO_COLL
-    coll.insert data
+    #TODO use something more sophisticated to loop through and sanitize data (build ruby-fu)
+    clean_properties = {}
+    data['properties'].each {|property, value| clean_properties[Sanitize.clean(property)] = Sanitize.clean(value)}
+    trackedData = {'event' => Sanitize.clean(data['event']), 'properties' => clean_properties, 'mpclone_time_tracked' => Time.now.to_i}
+    coll.insert trackedData
     result['status'] = 'success'
   end
   result.inspect
