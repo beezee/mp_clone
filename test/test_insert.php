85b97ba7df7c9f8672b0666fa1099b0482f4a29a
beezee <brian.zeligson@gmail.com>
2012-03-24 18:17:54 -0400

    format dates in popup

--- test/test_insert.php
+++ test/test_insert.php
@@ -3,5 +3,5 @@ require_once('mixpanel-api.php');
 
 $tracker = new MetricsTracker('chtkmpdemo');
 
-$tracker->track('click', array('state' => 'New Joisy', 'source' => 'google', 'ip' => '207.28.28.4'));
+$tracker->track('click', array('state' => 'Michigan', 'source' => 'yahoo', 'ip' => '207.28.28.4'));
 
